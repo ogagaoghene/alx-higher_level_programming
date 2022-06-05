df5f9f70f524b8dd6af52664d71e6ada6ef505cc
Esi Ogagaoghene <=esi.ogagaoghene@yahoo.com>
2022-06-05 17:44:08 +0000

    Add 2-infinite_add.py file that outputs the addition of all arguments passed to it

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
