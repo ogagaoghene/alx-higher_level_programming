080217a82fb0d1b10ba6a03912e99e23128767eb
Esi Ogagaoghene <=esi.ogagaoghene@yahoo.com>
2022-06-05 13:50:16 +0000

    Update 0-add.py file to include the simple file that contains the function to add two integers or numbers

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """Print the sum of 1 and 2 from a simple function add(a,b)"""
-    from add_integers import add
+    from add_0 import add
 
     a = 1
     b = 2
