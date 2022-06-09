9f60a47125ffe055c3a255af9dfdbf78eefcf526
Esi Ogagaoghene <=esi.ogagaoghene@yahoo.com>
2022-06-09 02:02:23 +0000

    Add 10-best_score.py file - a function that returns a key with the biggest integer value.

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
-    returned_value = list(a_dictionary.keys())[0]
-    biggest_number = a_dictionary[ret]
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
     for k, v in a_dictionary.items():
-        if v > biggest_number:
-            biggest_number = v
-            returned_value = k
-    return (returned_value)
+        if v > big:
+            big = v
+            ret = k
+    return (ret)
