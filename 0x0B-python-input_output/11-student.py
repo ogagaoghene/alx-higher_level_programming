40ccefa36dc02a140b8d93523e2b2c85f785f530
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-07-05 22:05:27 +0100

    Add 11-student.py file

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
-"""
-Contains the clas "Student"
-"""
+""" Student to JSON with filter. """
 
 
 class Student:
-    """Representation of a student"""
+    """ Define a student """
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """ Instation with first_name, last_name, age. """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """returns a dictionary representation of a Student instance"""
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """ Dictionary representation of a Student.
+        Return:
+            a dictionary representation of a Student instance
+            with specified attributes
+        """
+        if attrs is None:
+            return self.__dict__
 
+        dictt = {}
+        for i in attrs:
+            if i in self.__dict__:
+                dictt[i] = self.__dict__[i]
+        return dictt
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the Student instance. """
+        for key, value in json.items():
+            self.__dict__[key] = 
