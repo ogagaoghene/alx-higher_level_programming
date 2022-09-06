b69b554604d2484f64f972f9f9585a4bc880b70d
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-09-06 02:54:19 +0100

    Add 6-multi_languages_loop.js file

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
+'use strict';
+
 const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let index = 0; index < 3; index = index + 1) {
+for (let index = 0; index < 3; ++index) {
   console.log(arr[index]);
 }
