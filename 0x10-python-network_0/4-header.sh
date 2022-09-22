6574bdaaab34e2161b135c34dfc7fb34a8981b70
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-09-22 23:53:12 +0100

    Add 4-header.sh file

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
-# Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl "$1" -sX GET -H "X-School-User-Id:98"
+# #!/bin/bash
+# # Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+# curl "$1" -sX GET -H "X-School-User-Id:98"
 
+#!/bin/bash
+# script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -sX GET -H "X-School-User-Id: 98" "$1"
