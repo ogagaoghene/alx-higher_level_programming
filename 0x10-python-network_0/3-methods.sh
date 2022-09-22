0995b5ab209bbf899f00aa849b6eed50ffbdd96e
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-09-22 23:27:41 +0100

    Add 3-methods.sh file

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL and displays all HTTP methods the server will accept
-curl -sI "$1" | grep "Allow: " | sed 's/Allow: //'
+#takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep "Allow" | cut -d " " -f2-
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sd "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
+# takes in a URL, sends a POST request to the passed URL, displays the body of the response
+curl "$1" -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
