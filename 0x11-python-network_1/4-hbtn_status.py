d9e75b927bb3786347e4c44e6e99a4070d45ae0f
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-09-24 00:39:13 +0100

    Add: 4-hbtn_status.py file

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-""" Fetches https://intranet.hbtn.io/status using requests
-"""
+""" Fetches https://alx-intranet.hbtn.io/status using requests"""
 if __name__ == "__main__":
-    import requests
-    
-    r = requests.get('https://alx-intranet.hbtn.io/status', auth=('user', 'pass'))
-    print("Body response:")
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
+  import requests
+  r = requests.get('https://alx-intranet.hbtn.io/status', auth=('user', 'pass'))
+  print("Body response:")
+  print('\t- type: {}'.format(type(r.text)))
+  print('\t- content: {}'.format(r.text))
